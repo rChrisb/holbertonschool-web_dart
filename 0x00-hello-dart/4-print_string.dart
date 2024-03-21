6ad88386b7415cad2221145454b78fec3e574416
rChrisb <5767@holbertonstudents.com>
2024-03-21 18:45:32 +0100

    variables in dart

--- 0x00-hello-dart/4-print_string.dart
+++ 0x00-hello-dart/4-print_string.dart
@@ -1,6 +1,5 @@
 void main() {
   String str = "Holberton School";
   print(str * 3);
-  String lastNineChars = str.substring(0, 9);
-  print('$lastNineChars\n');
+  print(str.substring(0, 9)+'\n');
 }
